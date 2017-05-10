827dd0f383245c70193501bfa45a44fe7c32ab8f
Verhoelen Jonas <jonas.verhoelen@codecentric.de>
2017-05-11 01:09:07 +0200

    ..

--- src/test/java/de/jverhoelen/cryptoalerts/ingestion/subscriber/TrollboxSubscriberTest.java
+++ src/test/java/de/jverhoelen/cryptoalerts/ingestion/subscriber/TrollboxSubscriberTest.java
@@ -2,12 +2,15 @@ package de.jverhoelen.cryptoalerts.ingestion.subscriber;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TrollboxSubscriberTest {
     @Test
     public void containsTermAsWord() throws Exception {
-        boolean contained = TrollboxSubscriber.containsTermAsWord("I am a terrible programmer", "programmer");
-        assertTrue(contained);
+        assertTrue(TrollboxSubscriber.containsTermAsWord("I am a terrible programmer", "programmer"));
+        assertFalse(TrollboxSubscriber.containsTermAsWord("Litecoin is nice", "Lite"));
+        assertFalse(TrollboxSubscriber.containsTermAsWord("Litecoin is nice", "ni"));
+        assertFalse(TrollboxSubscriber.containsTermAsWord("Litecoin is nice", "ce"));
     }
 }
